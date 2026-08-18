ca6a9f00eb85e5304d7af8386fc3318a4c40b5ce
kn641655-maker <kn641655@gmail.com>
2026-08-18 11:12:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -115,7 +115,7 @@ formatted_agent_chain = (
 # --- 3. FastAPI App ---
 ##Need To Code
 app = FastAPI(title="Indian Weather and Cinema Agent")
-add_routs(app,
+add_routes(app,
           formatted_agent_chain,
           path="/agent",
           playground_type="dafalt")
